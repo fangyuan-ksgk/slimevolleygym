c54981c7781746705e2136ea101a61c5e878497a
fangyuan-ksgk <fangyuan.yu18@gmail.com>
2024-12-30 10:48:43 +0800

    update xor test

---
+++ neat/test_xor.py
@@ -0,0 +1,52 @@
+"""
+2-input XOR example -- this is most likely the simplest possible example.
+
+neat.nn.FeedForwardNetwork.create(genome, config)
+
+"""
+
+import neat
+
+# 2-input XOR inputs and expected outputs.
+xor_inputs = [(0.0, 0.0), (0.0, 1.0), (1.0, 0.0), (1.0, 1.0)]
+xor_outputs = [(0.0,), (1.0,), (1.0,), (0.0,)]
+
+
+def eval_genomes(genomes, config):
+    """ 
+    4 test cases, perfect fitness 4.0
+    sub-optimal solution subtracts from such perfect fitness score
+    sigmoid activated output [0, 1] target [0, 1], squared error [0, 1]
+    fitness range [0, 4]
+    """
+    for genome_id, genome in genomes:
+        genome.fitness = 4.0 
+        net = neat.nn.FeedForwardNetwork.create(genome, config)
+        for xi, xo in zip(xor_inputs, xor_outputs):
+            output = net.activate(xi)
+            genome.fitness -= (output[0] - xo[0]) ** 2 
+
+
+# Load configuration.
+config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction,
+                     neat.DefaultSpeciesSet, neat.DefaultStagnation,
+                     'config-feedforward')
+
+# Create the population, which is the top-level object for a NEAT run.
+p = neat.Population(config) # Create multiple genomes 
+
+# Add a stdout reporter to show progress in the terminal.
+p.add_reporter(neat.StdOutReporter(False))
+
+# Run until a solution is found.
+winner = p.run(eval_genomes) # genome list as well as config in population gets passed into eval_genomes to evaluate fitness on individual network
+
+# Display the winning genome.
+print('\nBest genome:\n{!s}'.format(winner))
+
+# Show output of the most fit genome against training data.
+print('\nOutput:')
+winner_net = neat.nn.FeedForwardNetwork.create(winner, config)
+for xi, xo in zip(xor_inputs, xor_outputs):
+    output = winner_net.activate(xi)
+    print("  input {!r}, expected output {!r}, got {!r}".format(xi, xo, output))
